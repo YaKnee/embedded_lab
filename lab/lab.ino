3c31b9fc045c19a3d002a5b9636cd6f815425c00
YaKnee <jani.oc@hotmail.com>
2024-04-05 14:49:24 +0300

    feat: clean version
    maybe change logic in future

--- lab/lab.ino
+++ lab/lab.ino
@@ -5,48 +5,70 @@
 
 #define fallingPin 3
 #define windDirPin A7
-#define MAC_6    0x73
-//#define MAC_6    0x62
-static uint8_t mymac[6] = { 0x44, 0x76, 0x58, 0x10, 0x00, MAC_6 };
-char bufa[80];
-char bufa2[80];
+
+//-----------------------------Server-----------------------
 byte server[] = { 10,6,0,21 }; 
 unsigned int Port = 1883;  
 EthernetClient ethClient; 
 PubSubClient client(server, Port, ethClient); 
 
-#define outTopic   "ICT4_out_2020" // Aihe, jolle viesti lähetetään
+#define outTopic   "ICT4_out_2020"
 int avgSpeed = 0;
 int avgDir = 0;
-char* clientId = "benguin24";
 boolean mqttSend = false; 
+char bufa[80];
 
 void send_MQTT_message() {
-    if (!client.connected()) { // Tarkistetaan onko yhteys MQTT-brokeriin muodostettu
-        connect_MQTT_server(); // Jos yhteyttä ei ollut, kutsutaan yhdistä -funktiota
+    if (!client.connected()) { 
+        connect_MQTT_server(); 
     }
-    if (client.connected()) { // Jos yhteys on muodostettu
+    if (client.connected()) { 
         if(mqttSend) {
           sprintf(bufa, "IOTJS={\"S_name1\":\"Benguin_windDir\",\"S_value1\":%d}", avgDir);
         } else {
           sprintf(bufa, "IOTJS={\"S_name1\":\"Benguin_windSpeed\",\"S_value1\":%d}", avgSpeed);
         }
-        Serial.println(bufa);
-        client.publish(outTopic, bufa); // Lähetetään viesti MQTT-brokerille
-        Serial.println("Message sent to MQTT server."); // Tulostetaan viesti onnistuneesta lähettämisestä
+        client.publish(outTopic, bufa);
+        Serial.println("Message sent to MQTT server."); 
     } else {
-        Serial.println("Failed to send message: not connected to MQTT server."); // Ei yhteyttä -> Yhteysvirheilmoitus
+        Serial.println("Failed to send message: not connected to MQTT server."); 
     }
 }
 
+char* clientId = "benguin24";
+
 void connect_MQTT_server() { 
-    Serial.println("Connecting to MQTT"); // Tulostetaan vähän info-viestiä
-    if (client.connect(clientId)) { // Tarkistetaan saadaanko yhteys MQTT-brokeriin
-        Serial.println("Connected OK"); // Yhdistetty onnistuneesti
+    Serial.println("Connecting to MQTT");
+    if (client.connect(clientId)) { 
+        Serial.println("Connected OK");
     } else {
-        Serial.println("Connection failed."); // Yhdistäminen epäonnistui
+        Serial.println("Connection failed."); 
     }    
 }
+
+//---------------------------------ETHERNET----------------------------
+static uint8_t mymac[6] = { 0x44, 0x76, 0x58, 0x10, 0x00, 0x73 };
+
+void fetchIP() {
+  byte connection = 1;
+  connection = Ethernet.begin(mymac);
+  Serial.print(F("\nW5100 Revision "));
+  if (connection == 0) {
+    Serial.println(F("Failed to access Ethernet controller"));
+  }
+  Serial.println(F("Setting up DHCP"));
+  Serial.print("Connected with IP: ");
+  Serial.println(Ethernet.localIP());
+  delay(1500);
+}
+
+
+//----------------------------BOARD--------------------------------------
+volatile unsigned long previousMillis = 0, displayMillis = 0, displayMillis2 = 0, pulseWidth = 0;
+double frequency = 0, totalSpeed = 0, totalDir = 0, windV = 0;
+
+boolean freqWindToggle = true;
+boolean degDirToggle = false;
 
 const byte ROWS = 1;
 const byte COLS = 4;
@@ -56,22 +78,6 @@ char hexaKeys[ROWS][COLS] = {
 byte rowPins[ROWS] = {A4}; 
 byte colPins[COLS] = {A0, A1, A2, A3}; 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-
-volatile unsigned long previousMillis = 0;
-volatile unsigned long displayMillis = 0;
-volatile unsigned long displayMillis2 = 0;
-volatile unsigned long pulseWidth = 0;
-
-volatile bool fallingEdgeDetected = false;
-double frequency = 0;
-double totalSpeed =0;
-double totalDir =0;
-
-boolean freqWindToggle = true;
-boolean degDirToggle = false;
-
-int analogVal = 0;
-double windV = 0;
 
 const int rs = 8, en = 6, d4 = 5, d5 = 4, d6 = 7, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -85,63 +91,63 @@ void setup() {
 }
 
 void loop() {
-  char customKey = customKeypad.getKey();
-  analogVal = analogRead(windDirPin);
-  windV = analogVal / 204.6;
+  windV = analogRead(windDirPin) / 204.6;
 
-  if(customKey == '1'){
-    freqWindToggle = !freqWindToggle;
-  } 
-  if(customKey == '2') {
-    degDirToggle = !degDirToggle; 
+  switch(customKeypad.getKey()) {
+      case '1':
+          //Change between frequency and speed
+          freqWindToggle = !freqWindToggle;
+          break;
+      case '2':
+          //Change between degrees and cardinal direction
+          degDirToggle = !degDirToggle;
+          break;
+      case '3':
+          //change between speed and direction for mqtt send
+          mqttSend = !mqttSend;
+          break;
+      case 'A':
+          //pippeli
+          displayD();
+          break;
   }
-  if(customKey == '3'){
-    mqttSend = !mqttSend;
-  }
-  if(customKey == 'A'){
-    displayD();
-  }
-
-  if (fallingEdgeDetected) {
-    volatile unsigned long currentMillis = millis();
-    pulseWidth = currentMillis - previousMillis;
-    previousMillis = currentMillis;
-    fallingEdgeDetected = false;
-    frequency = round(1000.0 / pulseWidth);
-  }
+  //Every 1 Second
   if(displayMillis+1000 <= millis()){
       totalSpeed += freqToWindSpeed(frequency);
       totalDir += voltToDeg (windV);
       displayLCD();
       displayMillis = millis();
   }
-   if(displayMillis2+5000 <= millis()){
+  //Every 5 Seconds
+  if(displayMillis2+5000 <= millis()){
       avgSpeed = round(totalSpeed/5);
       avgDir = round(totalDir/5);
-      Serial.println("Dir:" + String(avgDir));
       clearSpecificPart(3);
       lcd.setCursor(0,3);
       lcd.print("S: " + String(avgSpeed) + " D: " + String(avgDir));
       displayMillis2 = millis();
-      totalSpeed =0;
-      totalDir =0;
       send_MQTT_message();
+      totalSpeed = 0;
+      totalDir = 0;
       avgSpeed = 0;
       avgDir = 0;
   }
 }
 
+void detectFalling() {
+  volatile unsigned long currentMillis = millis();
+  pulseWidth = currentMillis - previousMillis;
+  previousMillis = currentMillis;
+  frequency = round(1000.0 / pulseWidth);
+}
+
 void displayLCD() {
-   //lcd.clear();
    clearSpecificPart(0);
    clearSpecificPart(1);
-   //clearSpecificPart(2);
    lcd.setCursor(0,0);
    if (freqWindToggle) {
-      // Display speed
       lcd.print("Spd.: " + String(freqToWindSpeed(frequency)) + "ms " + String(windV) + "V");
    } else {
-      // Display frequency
       lcd.print("Freq: " + String(frequency) + "Hz " + String(windV) + "V");
    }
    lcd.setCursor(0,1);
@@ -151,13 +157,13 @@ void displayLCD() {
    } else {
       lcd.print(String(voltToDir(windV)));
    }
+   lcd.setCursor(0,2);
+   lcd.print(Ethernet.localIP());
 }
 
 double freqToWindSpeed(double freq) {
-  //return ((0.0019*pow(freq, 3))-(0.0936*pow(freq,2)) + (1.9827*freq) - 5.0893);
   return -0.24+(freq * 0.699);
 }
-
 
 int voltToDeg(double windV) {
   if (windV <= 1.2) return 0;
@@ -167,44 +173,19 @@ int voltToDeg(double windV) {
 
 String voltToDir(double windV){
     String directions[] = {"N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"};
-    //int index = (int)(floor((voltToDeg(windV)) + 22.5) / 45) % 8;
     int index = round((voltToDeg(windV)) / 45);
     return directions[index];
 }
-void averageSpeedDir(double totalSpeed, double totalDir){
-    clearSpecificPart(3);
-    lcd.setCursor(0,3);
-    lcd.print("S: " + String(totalSpeed/5) + " D: " + String(totalDir/5));
-}
 
-void detectFalling() {
-  fallingEdgeDetected = true;
-}
-void fetchIP() {
-  byte connection = 1;
-  connection = Ethernet.begin(mymac);
-  
-  Serial.print(F("\nW5100 Revision "));
-  if (connection == 0) {
-    Serial.println(F("Failed to access Ethernet controller"));
-  }
-  
-  Serial.println(F("Setting up DHCP"));
-  Serial.print("Connected with IP: ");
-  Serial.println(Ethernet.localIP());
-     lcd.setCursor(0,2);
-   //lcd.print("IP: " + String(Ethernet.localIP()));
-   lcd.print(Ethernet.localIP());
-  delay(1500);
-}
 void clearSpecificPart(int row) {
   lcd.setCursor(0, row);
   for (int i = 0; i <= 19; i++) {
-    lcd.print(' '); // Print a space character to clear the part
+    lcd.print(' ');
   }
 }
+
 void displayD(){
-          byte customCharacters[8][8] = {
+     byte customCharacters[8][8] = {
         {B00111, B01000, B10000, B10000, B10000, B10000, B01000, B00110}, // bU1
         {B00110, B01000, B10000, B10000, B10000, B10000, B01000, B00111}, // bD1
         {B00000, B10000, B01000, B01000, B01000, B01111, B10000, B00000}, // bU2
@@ -215,7 +196,6 @@ void displayD(){
         {B00011, B00001, B10011, B10010, B01100, B00000, B00000, B00000}, // tD1
     };
     for(int j =0; j< 16; j++){
-//    lcd.clear();
     clearSpecificPart(0);
     clearSpecificPart(1);
     int positions[8][2] = {{j, 0},{j, 1}, {j+1, 0},{j+1, 1},{j+2, 0},{j+2, 1},{j+3, 0},{j+3, 1}};
