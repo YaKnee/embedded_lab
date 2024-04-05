9f5229483916d4953783fb62699a255e91647c9d
YaKnee <jani.oc@hotmail.com>
2024-04-05 12:53:51 +0300

    feat: FUCKING WORKS

--- lab/lab.ino
+++ lab/lab.ino
@@ -8,28 +8,24 @@
 #define MAC_6    0x73
 //#define MAC_6    0x62
 static uint8_t mymac[6] = { 0x44, 0x76, 0x58, 0x10, 0x00, MAC_6 };
-
+char bufa[80] = "Hello Benguin";
 byte server[] = { 10,6,0,21 }; // MQTT-palvelimen IP-osoite
 unsigned int Port = 1883;  // MQTT-palvelimen portti
 EthernetClient ethClient; // Ethernet-kirjaston client-olio
-void callback(char* topic, byte* payload, unsigned int length);
 PubSubClient client(server, Port, ethClient); // PubSubClient-olion luominen
 
 #define outTopic   "ICT4_out_2020" // Aihe, jolle viesti lähetetään
-char bufa[50];
-char* clientId = "aaaaaaa"; // MQTT-clientin tunniste
-char* deviceId = "Benguin24";
-char* deviceSecret = "tamk";
-
+int avgSpeed = 0;
 int avgDir = 0;
-double avgSpeed = 0;
+char* clientId = "benguin24"; 
 
 void send_MQTT_message() {
     if (!client.connected()) { // Tarkistetaan onko yhteys MQTT-brokeriin muodostettu
         connect_MQTT_server(); // Jos yhteyttä ei ollut, kutsutaan yhdistä -funktiota
     }
     if (client.connected()) { // Jos yhteys on muodostettu
-        sprintf(bufa, "IOTJS={\"S_name1\":\”Benguin_windSpeed\",\"S_value1\":%0.2f}", avgSpeed);
+        sprintf(bufa, "IOTJS={\"S_name1\":\"Benguin_windSpeed\",\"S_value1\":%d}", avgSpeed);
+        Serial.println(bufa);
         client.publish(outTopic, bufa); // Lähetetään viesti MQTT-brokerille
         Serial.println("Message sent to MQTT server."); // Tulostetaan viesti onnistuneesta lähettämisestä
     } else {
@@ -39,22 +35,15 @@ void send_MQTT_message() {
 
 void connect_MQTT_server() { 
     Serial.println("Connecting to MQTT"); // Tulostetaan vähän info-viestiä
-    if (client.connect(clientId, deviceId, deviceSecret)) { // Tarkistetaan saadaanko yhteys MQTT-brokeriin
+    if (client.connect(clientId)) { // Tarkistetaan saadaanko yhteys MQTT-brokeriin
         Serial.println("Connected OK"); // Yhdistetty onnistuneesti
     } else {
         Serial.println("Connection failed."); // Yhdistäminen epäonnistui
     }    
 }
 
-
 const byte ROWS = 1;
 const byte COLS = 4;
-//char hexaKeys[ROWS][COLS] = {
-//  {'1', '2', '3', 'A'},
-//  {'4', '5', '6', 'B'},
-//  {'7', '8', '9', 'C'},
-//  {'*', '0', '#', 'D'}
-//};
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
 };
@@ -76,7 +65,6 @@ boolean freqWindToggle = true;
 boolean degDirToggle = false;
 int analogVal = 0;
 double windV = 0;
-
 
 const int rs = 8, en = 6, d4 = 5, d5 = 4, d6 = 7, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -118,23 +106,25 @@ void loop() {
       displayMillis = millis();
   }
    if(displayMillis2+5000 <= millis()){
+      avgSpeed = round(totalSpeed/5);
+      avgDir = round(totalDir/5);
       clearSpecificPart(3);
       lcd.setCursor(0,3);
-      avgSpeed = totalSpeed/5;
-      avgDir = round(totalDir/5);
       lcd.print("S: " + String(avgSpeed) + " D: " + String(avgDir));
       displayMillis2 = millis();
       totalSpeed =0;
       totalDir =0;
-      send_MQTT_message(); // Kutsutaan MQTT-viestin lähettämis-funktiota
+      send_MQTT_message();
       avgSpeed = 0;
       avgDir = 0;
   }
 }
 
 void displayLCD() {
+   //lcd.clear();
    clearSpecificPart(0);
    clearSpecificPart(1);
+   //clearSpecificPart(2);
    lcd.setCursor(0,0);
    if (freqWindToggle) {
       // Display speed
@@ -166,6 +156,7 @@ int voltToDeg(double windV) {
 
 String voltToDir(double windV){
     String directions[] = {"N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"};
+    //int index = (int)(floor((voltToDeg(windV)) + 22.5) / 45) % 8;
     int index = round((voltToDeg(windV)) / 45);
     return directions[index];
 }
@@ -190,20 +181,17 @@ void fetchIP() {
   Serial.println(F("Setting up DHCP"));
   Serial.print("Connected with IP: ");
   Serial.println(Ethernet.localIP());
-  lcd.setCursor(0,2);
-  lcd.print(Ethernet.localIP());
+     lcd.setCursor(0,2);
+   //lcd.print("IP: " + String(Ethernet.localIP()));
+   lcd.print(Ethernet.localIP());
   delay(1500);
 }
-
-
-
 void clearSpecificPart(int row) {
   lcd.setCursor(0, row);
   for (int i = 0; i <= 19; i++) {
     lcd.print(' '); // Print a space character to clear the part
   }
 }
-
 void displayD(){
           byte customCharacters[8][8] = {
         {B00111, B01000, B10000, B10000, B10000, B10000, B01000, B00110}, // bU1
@@ -216,8 +204,7 @@ void displayD(){
         {B00011, B00001, B10011, B10010, B01100, B00000, B00000, B00000}, // tD1
     };
     for(int j =0; j< 16; j++){
-    clearSpecificPart(0);
-    clearSpecificPart(1);
+    lcd.clear();
     int positions[8][2] = {{j, 0},{j, 1}, {j+1, 0},{j+1, 1},{j+2, 0},{j+2, 1},{j+3, 0},{j+3, 1}};
     for (int i = 0; i < 8; i++) {
         lcd.createChar(i, customCharacters[i]);
@@ -225,42 +212,5 @@ void displayD(){
         lcd.write((char)i);
     }
     delay(300);
-    }
-}
-
-void thumbUp(){
-    lcd.clear();
-    byte thumbCharacters[6][8] = {
-        {B00100, B00011, B00100, B00011, B00100, B00011, B00010, B00001},
-        {B00000, B00000, B00000, B00000, B00000, B00000, B00000, B00011},
-        {B00000, B00000, B00000, B00000, B00000, B00000, B00001, B11110},
-        {B00000, B01100, B10010, B10010, B10001, B01000, B11110, B00000},
-        {B00010, B00010, B00010, B00010, B00010, B01110, B10000, B00000},
-        {B00000, B00000, B00000, B00000, B00000, B10000, B01000, B00110}
-    };
-
-    int positions[6][2] = {{4, 1},{4, 0},{5, 1},{5, 0},{6, 1},{6, 0}};
-    for (int i = 0; i < 6; i++) {
-        lcd.createChar(i, thumbCharacters[i]);
-        lcd.setCursor(positions[i][0], positions[i][1]);
-        lcd.write((char)i);
-    }
-}
-void thumbDown(){
-    lcd.clear();
-    byte thumbCharacters[6][8] = {
-        {B00001, B00010, B00011, B00100, B00011, B00100, B00011, B00100}, 
-        {B00011, B00000, B00000, B00000, B00000, B00000, B00000, B00000},
-        {B11110, B00001, B00000, B00000, B00000, B00000, B00000, B00000},
-        {B00000, B11110, B01000, B10001, B10010, B10010, B01100, B00000},
-        {B00000, B10000, B01110, B00010, B00010, B00010, B00010, B00010},
-        {B00110, B01000, B10000, B00000, B00000, B00000, B00000, B00000}
-    };
-
-    int positions[6][2] = {{4, 0},{4, 1},{5, 0},{5, 1},{6, 0},{6, 1}};
-    for (int i = 0; i < 6; i++) {
-        lcd.createChar(i, thumbCharacters[i]);
-        lcd.setCursor(positions[i][0], positions[i][1]);
-        lcd.write((char)i);
     }
 }
